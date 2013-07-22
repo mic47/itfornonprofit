b6247c7f025bb2d0cb9fdf0ee4a8c54be7ab16a3
Michal Nanasi <michaln@michaln-mbp2.local>
2013-07-23 00:27:32 +0100

    better url handling

--- summerofhack/urls.py
+++ summerofhack/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns(
     url(r'^itfornonprofits/', include('itfornonprofits.urls')),
     url(r'^admin/', include(admin.site.urls)),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    url(r'^', include('itfornonprofits.urls')),
 )
